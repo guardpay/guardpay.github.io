14f0b26e9c9424391d8c7f6a56e3a9abfcdfaf77
Scan Pay <scanpayasia@gmail.com>
2018-01-31 16:07:10 +0700

    Update

--- static/js/main.fae7c5d0.js
+++ static/js/main.fae7c5d0.js
@@ -12913,46 +12913,7 @@
                             id: "readme"
                         }), l.default.createElement("p", {
                             className: "center"
-                        }, "Video guide to using dashboard", l.default.createElement("br", null), l.default.createElement("iframe", {
-                            width: "560",
-                            height: "315",
-                            src: "https://www.youtube.com/embed/T2_XzSlddWI?rel=0",
-                            frameborder: "0",
-                            allowfullscreen: !0
-                        }), l.default.createElement("br", null), l.default.createElement("br", null)))), l.default.createElement("div", {
-                            className: "white-bg2"
-                        }, l.default.createElement("div", {
-                            className: "narrow-body-container"
-                        }, l.default.createElement("br", null), l.default.createElement("br", null), l.default.createElement("h2", {
-                            className: "center"
-                        }, "For Developers"), l.default.createElement("br", null), l.default.createElement("p", {
-                            className: "center"
-                        }, "Links to the open source contract code, bug bounty program and github."), l.default.createElement("br", null), l.default.createElement("table", {
-                            className: "home"
-                        }, l.default.createElement("tr", null, l.default.createElement("td", null, l.default.createElement("a", {
-                            href: "https://etherscan.io/address/0x1c02ce498dc6d0d6ef05a253e021258b07eeba91",
-                            target: "_blank"
-                        }, l.default.createElement("button", {
-                            className: "HomeButton"
-                        }, "Contract Source Code"))), l.default.createElement("td", null), l.default.createElement("td", null, l.default.createElement("a", {
-                            href: "https://github.com/KayinCheung/KayinCheung.github.io",
-                            target: "_blank"
-                        }, l.default.createElement("button", {
-                            className: "HomeButton"
-                        }, "Github Repository"))), l.default.createElement("td", null), l.default.createElement("td", null, l.default.createElement("a", {
-                            href: "https://www.reddit.com/r/ethdev/comments/6zwb1x/escrowmyether_1_eth_bug_bounty/",
-                            target: "_blank"
-                        }, l.default.createElement("button", {
-                            className: "HomeButton"
-                        }, "Bug Bounty"))))))), l.default.createElement("div", {
-                            className: "bg-lb"
-                        }, l.default.createElement("div", {
-                            className: "narrow-body-container"
-                        }, l.default.createElement("br", null), l.default.createElement("br", null), l.default.createElement("h2", {
-                            className: "center"
-                        }, "Contact"), l.default.createElement("br", null), l.default.createElement("p", {
-                            className: "center"
-                        }, "hi@scanpay.asia"), l.default.createElement("br", null)))))
+                        }, l.default.createElement("br", null)))))
                     }
                 }]), t
             }(u.Component));
@@ -17462,7 +17423,7 @@
                         className: "leftlogo"
                     }, l.default.createElement("h2", null, l.default.createElement(y.IndexLink, {
                         to: "/"
-                    }, "Escrow Scan Pay")))), t, e, this.props.children || l.default.createElement(b.default, null))
+                    }, "Scan Pay")))), t, e, this.props.children || l.default.createElement(b.default, null))
                 }
             }]), t
         }(u.Component);
